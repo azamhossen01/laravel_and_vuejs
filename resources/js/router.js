50aafd4d46cead797ba05ac849c31b5f89997b69
azamhossen01 <azamhossen01@gmail.com>
2020-03-02 18:43:03 +0600

    first commit

---
+++ resources/js/router.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+const routes = [
+    { path: '/dashboard', component: require('./components/Dashboard.vue').default },
+    { path: '/profile', component: require('./components/Profile.vue').default },
+    { path: '/users', component: require('./components/Users.vue').default }
+  ];
+
+  export default  new VueRouter({
+    mode:'history',
+    routes // short for `routes: routes`
+  })
+
+  
